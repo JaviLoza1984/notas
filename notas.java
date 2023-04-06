05daa15f578032ea865b8d6001c7b06662a7b3f0
someone <someone@someplace.com>
2023-04-06 14:34:35 +0200

    Cambio de acumulados

--- notas.java
+++ notas.java
@@ -13,8 +13,8 @@ public class notas {
 	 */
 	
 	
-	double uf4, uf5, uf6;
-	double acu1, acu2, acu3, def;
+	double uf1, uf2, uf3;
+	double acu4, acu5, acu6, def;
 	
 	/**
 	 * Solicita al usuario las notas a introducir
@@ -32,16 +32,16 @@ public class notas {
 		
 		System.out.print("ingrese nota 1: ");
 		
-		uf4= entrada.nextDouble();
+		uf1= entrada.nextDouble();
 
 		
 		
 		System.out.print("ingrese nota 2: ");
 		
-		uf5= entrada.nextDouble();
+		uf2= entrada.nextDouble();
 		System.out.print("ingrese nota 3: ");
 		
-		uf6= entrada.nextDouble();
+		uf3= entrada.nextDouble();
 		
 	
 	}
@@ -53,20 +53,20 @@ public class notas {
 	
 	public void comprobarcion(){
 		
-		if (uf4>10) {
+		if (uf1>10) {
 			System.out.println(" nota1 mal introducida");
 			
 		}else {
 			System.out.println(" nota1 correcta");
 		}
 		
-		if (uf5>10) {
+		if (uf2>10) {
 			System.out.println(" nota2 mal introducida");
 			
 		}else {
 			System.out.println(" nota2 correcta");
 		}
-		if (uf6>10) {
+		if (uf3>10) {
 			System.out.println(" nota3 mal introducida");
 			
 		}else {
@@ -84,11 +84,11 @@ public class notas {
 	
 	
 	public void Calculonotas() {
-		acu1= uf4*0.35;
-		acu2 = uf5 * 0.35;
-		acu3 = uf6 * 0.30;
+		acu4= uf1*0.35;
+		acu5 = uf2 * 0.35;
+		acu6 = uf3 * 0.30;
 		
-		def = acu1 + acu2+ acu3;
+		def = acu4 + acu5+ acu6;
 		
 		
 	}
@@ -100,13 +100,13 @@ public class notas {
 	public void Mostrar() {
 		
 		System.out.println(" notas introducidas son:");
-		System.out.println(" nota1 = " + uf4);
-		System.out.println(" nota2 = " + uf5);
-		System.out.println(" nota3 = " + uf6);
+		System.out.println(" nota1 = " + uf1);
+		System.out.println(" nota2 = " + uf2);
+		System.out.println(" nota3 = " + uf3);
 		
-		System.out.println(" acumuado 1 = "+ acu1);
-		System.out.println(" acumuado 2 = "+ acu2);
-		System.out.println(" acumuado 3 = "+ acu3);
+		System.out.println(" acumuado 1 = "+ acu4);
+		System.out.println(" acumuado 2 = "+ acu5);
+		System.out.println(" acumuado 3 = "+ acu6);
 		
 		System.out.println(" nota definitiva es = "+ def);
 		
